c3fee49fa8be0abbc272dd93eb989b4f33f52ff2
Matias Di Iorio <matiasmd8610@gmail.com>
2024-08-11 17:38:27 -0300

    Update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
     output : "export",
     reactStrictMode: true,
-    basePath: isProd ? '/kerlin_campos': '',
     images: {
         unoptimized: true
     }
