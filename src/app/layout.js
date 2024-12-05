c867164e6b5039a7f0da162240279ac14febbc2c
Matias Di Iorio <matiasmd8610@gmail.com>
2024-12-04 21:16:52 -0300

    Add google font by nextjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Montserrat } from 'next/font/google';
 import "../styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -12,11 +13,13 @@ export const metadata = {
   },
 };
 
+const montserrat = Montserrat({ subsets: ['latin'] })
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <GoogleAnalytics />
-      <body>
+      <body className={montserrat.className}>
         <Header />
         {children}
         <Footer />
